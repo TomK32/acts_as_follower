69cdb2573b5e4651c4b658a45128a1940cdb5a48
Jörg Battermann <jb@joergbattermann.com>
2008-12-28 18:02:58 +0100

    transitioning follow.rb to be a template from now on, no longer a real model in the plugin's lib folder

--- generators/acts_as_follower_migration/acts_as_follower_migration_generator.rb
+++ generators/acts_as_follower_migration/acts_as_follower_migration_generator.rb
@@ -1,6 +1,9 @@
 class ActsAsFollowerMigrationGenerator < Rails::Generator::Base
   def manifest
     record do |m| 
+      m.directory "app/models"
+      m.template "model.rb", "app/models/follow.rb"
+      
       m.migration_template 'migration.rb', 'db/migrate'
     end
   end
